069384274fb4fa31eb448edb3d8f2a91e724b000
unknown <NTTUyen0111>
2024-03-20 21:58:19 +0700

    20/03

---
+++ Chapter10/Analyze.js
@@ -0,0 +1,22 @@
+function pic1(){
+    var figElement = document.getElementById("showimg");
+    figElement.style.backgroundImage = "url('https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3NrOTc5MS1pbWFnZS1rd3Z1amE5Ni5qcGc.jpg')";
+    figElement.style.backgroundSize = "400px 400px";
+    figElement.style.width = "400px";
+    figElement.style.height = "400px";
+}
+
+function pic2(){
+    var imgSource = document.getElementById("showimg");
+    imgSource.style.backgroundImage = "url('https://t3.ftcdn.net/jpg/06/06/79/70/360_F_606797008_rGPPk6bFWDQydnX7g7w1w9dVVZ4mD22J.jpg')";
+}
+
+function pic3(){
+    var figCap = document.getElementById("showimg");
+    figCap.style.backgroundImage = "url('https://wallpapers.com/images/featured/beautiful-scenery-wnxju2647uqrcccv.jpg')";
+}
+
+function text(){
+    var text = document.getElementById("text");
+    text.textContent ="Trunk Bay in St. John";
+}
---
+++ Chapter10/Extend.js
@@ -0,0 +1,8 @@
+
+document.getElementById('show').addEventListener('click', function() {
+    document.getElementById('text').style.display = 'block';
+  });
+  
+  document.getElementById('hide').addEventListener('click', function() {
+    document.getElementById('text').style.display = 'none';
+  });
